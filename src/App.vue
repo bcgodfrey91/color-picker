961c1a591866a3feb2483cd73461ae165aebef77
Ben Godfrey <bcgodfrey91@gmail.com>
2017-09-15 22:35:35 -0500

    add helper file for colors

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
     <div class="color-picker-container">
-      <div class="color-column"></div>
-      <div class="color-results-column"></div>
+      <div class="color-column">
+        <div class="colors-container" v-for="color in colors" :key="color.color">
+          {{color.color}}
+        </div>
+      </div>
+      <div class="color-variations"></div>
     </div>
   </div>
 </template>
 
 <script>
+import colors from './colors.json';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      colors: colors.materialColors,
+    };
+  },
 };
 </script>
 
@@ -38,8 +48,24 @@ body {
   height: 45rem;
   width: 30rem;
 
-  border: 1px solid black;
   border-radius: 0.5rem;
+
+  display: flex;
+  flex-direction: row;
+}
+
+.color-column,
+.color-variations {
+  border: 1px solid black;
+  height: 100%;
+}
+
+.color-column {
+  flex: 1;
+}
+
+.color-variations {
+  flex: 6;
 }
 
 </style>
